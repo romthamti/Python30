f6965312c48ad927e08618d2f662c4aaa914b12d
romthamti <romtham.ti@gmail.com>
2026-02-09 23:22:51 +0700

    update 22

--- 22_water_goal.py
+++ 22_water_goal.py
@@ -1,13 +1,17 @@
-# รับค่าน้ำหนักตัว
-weight = float(input("น้ำหนักตัว (กิโลกรัม): "))
+weight = float(input("ระบุน้ำหนักตัว: "))
 
-# คำนวณปริมาณน้ำที่ควรดื่ม (มล.)
-water_ml = weight * 33
-
-# แปลงจากมิลลิลิตรเป็นลิตร
+water_ml = weight*33
 water_liter = water_ml / 1000
 
-# แสดงผล
 print("ควรดื่มน้ำประมาณ {:.0f} มล. หรือ {:.2f} ลิตร/วัน".format(
     water_ml, water_liter
 ))
+
+goal = float(input("ระบุจำนวนที่ดื่มไปแล้ววันนี้(มล): "))
+
+if goal < water_ml:
+    print("ดื้มน้ำยังไม่พอ")
+elif goal <= water_ml +500:
+    print("ยอดเยี่ยม")
+else:
+    print("ดื้มน้ำเยอะเกินไป")
